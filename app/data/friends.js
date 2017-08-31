312267f755cf40381b75d4461b6c5c88ec465785
Almira1612 <almira1612@gmail.com>
2017-08-31 12:44:18 -0400

    finish

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friends =[
+  {
+  "name":"Jerry",
+  "photo":"http://i.huffpost.com/gen/781204/images/o-CUTE-ANIMALS-PHOTOS-facebook.jpg",
+  "scores":[
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+   },
+   {
+"name": "Lisa",
+"photo": "https://i.pinimg.com/736x/e1/66/83/e1668355c8c5a68167b313837311d40f--happy-hedgehog-baby-hedgehog-cute.jpg",
+"scores": [
+      "4",
+      "5",
+      "3",
+      "4",
+      "5",
+      "3",
+      "3",
+      "2",
+      "5",
+      "4"
+    ]
+  },
+{
+"name": "Mariya",
+"photo": "http://s11.favim.com/mini/170328/baby-animals-cats-cute-animals-kitten-Favim.com-5140341.jpeg",
+"scores": [
+      "1",
+      "2",
+      "1",
+      "2",
+      "3",
+      "3",
+      "1",
+      "2",
+      "3",
+      "1"
+    ]
+  }
+]
+module.exports = friends;
--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-// Coding coding coding......................
+// Router
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 
